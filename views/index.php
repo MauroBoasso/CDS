1b302a4f4a005ba58ebfe15c3733f2f25597c2eb
Nicolás Springer <nicosspringer16@gmail.com>
2023-08-01 01:01:39 -0300

    agregar js, css, php

---
+++ views/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ejemplo de PHP</title>
+</head>
+<body>
+
+<?php
+// Este es un comentario de una sola línea en PHP
+
+// Definimos una variable con el mensaje que queremos mostrar
+$mensaje = "¡Hola, este es un ejemplo de PHP!";
+
+// Imprimimos el mensaje en el navegador
+echo $mensaje;
+?>
+
+</body>
+</html>
